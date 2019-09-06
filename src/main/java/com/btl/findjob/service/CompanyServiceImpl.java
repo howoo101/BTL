d3eb4dfd12fc47efe11e7ed59fbca2dd3f1d09ab
hohoho11 <ksu1346@naver.com>
2019-09-06 18:48:48 +0900

    검색 리스트 화면

--- src/main/java/com/btl/findjob/service/CompanyServiceImpl.java
+++ src/main/java/com/btl/findjob/service/CompanyServiceImpl.java
@@ -3,12 +3,13 @@ package com.btl.findjob.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.btl.findjob.mapper.CompanyMapper;
 import com.btl.findjob.model.CompanyInfoDTO;
 
+@Service
 public class CompanyServiceImpl implements CompanyService {
-
 	
 	@Autowired
 	CompanyMapper mapper;
