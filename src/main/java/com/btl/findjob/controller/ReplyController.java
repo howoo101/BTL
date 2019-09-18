61d8a947eab5e2560d4530fcc526a509b8e877cc
uru1217 <uru1217@naver.com>
2019-09-18 17:27:01 +0900

    댓글 등록,수정,삭제 js파일 생성

---
+++ src/main/java/com/btl/findjob/controller/ReplyController.java
@@ -0,0 +1,84 @@
+package com.btl.findjob.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.btl.findjob.model.BoardCriteria;
+import com.btl.findjob.model.ReplyDTO;
+import com.btl.findjob.service.ReplyService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@RequestMapping("replies/")
+@RestController
+@Log4j
+@AllArgsConstructor
+public class ReplyController {
+    private ReplyService service;
+    
+    @PostMapping(value="/new", consumes="application/json", produces= {MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<String> create(@RequestBody ReplyDTO dto) {
+        log.info("ReplyDTO:"+ dto);
+        
+        int insertCount = service.replyRegister(dto);
+        
+        log.info("Reply INSERT COUNT :"+insertCount);
+        
+        System.out.println("new : 구문 이쪽으로: delete 구문");
+        
+        return insertCount == 1 
+        ? new ResponseEntity<>("success", HttpStatus.OK) 
+        : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        
+    }
+    
+    @GetMapping(value="/pages/{board_id}/{page}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<List<ReplyDTO>> getList(@PathVariable("page") int page, @PathVariable("board_id") int board_id) {
+       log.info("getList...........");
+       BoardCriteria cri = new BoardCriteria(page, 10);
+       log.info(cri);
+        return new ResponseEntity<>(service.replyGetList(cri, board_id),HttpStatus.OK);
+    }
+    
+    @GetMapping(value="/{reply_id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<ReplyDTO> get(@PathVariable("reply_id") int reply_id) {
+        log.info("get:" + reply_id);
+        return new ResponseEntity<>(service.replyGet(reply_id),HttpStatus.OK);
+    }
+    
+    @DeleteMapping(value = "/{reply_id}", produces = {MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<String> remove(@PathVariable("reply_id") int reply_id){
+
+        log.info("remove: "+reply_id);
+
+        return service.replyRemove(reply_id) ==1 
+                ? new ResponseEntity<>("success",HttpStatus.OK) 
+                : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+     
+     
+     
+    @RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH},
+            value="/{reply_id}", consumes = "application/json", produces= {MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<String> modify(@RequestBody ReplyDTO dto, @PathVariable("reply_id") int reply_id) {
+        dto.setReply_id(reply_id);
+        log.info("reply_id..."+reply_id);
+        log.info("modify:"+dto);
+        return service.replyModify(dto) ==1 
+                ? new ResponseEntity<>("success",HttpStatus.OK) 
+                : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ src/main/java/com/btl/findjob/mapper/ReplyMapper.java
@@ -0,0 +1,20 @@
+package com.btl.findjob.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.btl.findjob.model.BoardCriteria;
+import com.btl.findjob.model.ReplyDTO;
+
+public interface ReplyMapper {
+    public ReplyDTO read(int board_id);
+    public int insert(ReplyDTO dto);
+    public int update(ReplyDTO dto);
+    public int delete(int reply_id);
+    public List<ReplyDTO> getListWithPaging(
+            @Param("cri")  BoardCriteria cri,
+            @Param("board_id") int board_id);
+    public int count(int board_id);
+    public int getBoard_id(int reply_id);
+}
